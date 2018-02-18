84469a1360bd4de880c4cf6f7945e759763649df
Yufan Wang <yufansmail@gmail.com>
2018-02-17 17:27:24 -0800

    deploy test

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var path = require('path');
 var app = express();
 var PORT = process.env.PORT || 8081;
+var mDatabase = process.env.MONGOLAB_URI || 'mongodb://localhost:27017/comics';
 var cors = require('cors');
 var morgan = require('morgan');
 var mongoose = require('mongoose');
@@ -16,7 +17,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + '/src'));
 app.use(appRoutes);
 
-mongoose.connect('mongodb://localhost:27017/comics', function(err) {
+mongoose.connect(mDatabase, function(err) {
   if (err) {
     console.log('MongoDB not connected', err);
   } else {
