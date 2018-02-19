a3154b0619aa6666009d25e10c594b01ff17c5b2
Yufan Wang <yufansmail@gmail.com>
2018-02-19 00:40:05 -0800

    Allows enter for submitting comment

--- src/app/commentsform.js
+++ src/app/commentsform.js
@@ -1,12 +1,21 @@
 angular.module('app')
 .controller('formCtrl', function() {
   this.handleClick = () => {
+    this.postText();
+    this.commentText = '';
+  };
+
+  this.handleKeyUp = () => {
+    this.postText();
+    this.commentText = '';
+  };
+
+  this.postText = () => {
     if (this.commentText) {
       let currentNum = this.comic.num;
       this.service.postComment(this.commentText, currentNum, this.comments);
     }
-    this.commentText = '';
-  }
+  };
 })
 .component('commentsForm', {
   controller: 'formCtrl',
