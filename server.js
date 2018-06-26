03b365fd02c839de80e0fa495e022f2a91558d57
Yufan Wang <yufanwangcn@hotmail.com>
2018-06-26 16:33:21 -0700

    restyling and refactored entire site, removed bootstrap

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var path = require("path");
 var app = express();
-var PORT = process.env.PORT || 8080;
+var PORT = process.env.PORT || 3000;
 var mDatabase =
   process.env.MONGODB_URI ||
   "mongodb://heroku_98wj9g94:mtalumb98b3emmejo9iq7uceat@ds239648.mlab.com:39648/heroku_98wj9g94";
@@ -14,13 +14,16 @@ app.use(cors());
 app.use(express.static(__dirname + "/src"));
 app.use("/api", appRoutes);
 
-mongoose.connect(mDatabase, function(err) {
-  if (err) {
-    console.log("MongoDB not connected", err);
-  } else {
-    console.log("Connected to database");
+mongoose.connect(
+  mDatabase,
+  function(err) {
+    if (err) {
+      console.log("MongoDB not connected", err);
+    } else {
+      console.log("Connected to database");
+    }
   }
-});
+);
 
 app.get("*", function(req, res, next) {
   res.sendFile(path.join(__dirname + "/src/index.html"));
--- src/app/app.js
+++ src/app/app.js
@@ -4,15 +4,14 @@ angular
     $sceDelegateProvider.resourceUrlWhitelist(["self", "https://xkcd.com/**"]);
   })
   .controller("AppCtrl", function(getComic) {
-    this.default = {
+    this.home = {
       img: "https://imgs.xkcd.com/comics/types.png",
       num: 1537,
       title: "Types",
       year: "2015",
       alt: "My new language is great, but it has a few quirks"
     };
-    this.currentComic = this.default;
-    this.home = this.default;
+    this.currentComic = this.home;
     this.getcomic = getComic;
     this.commentText = "";
     this.commentName = "";
@@ -30,14 +29,6 @@ angular
     this.goHome = () => {
       this.currentComic = this.home;
       this.setComments();
-    };
-
-    this.setHome = () => {
-      this.home = this.currentComic;
-    };
-
-    this.resetHome = () => {
-      this.home = this.default;
     };
 
     this.setComments = () => {
